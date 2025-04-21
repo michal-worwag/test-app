6cd8d2c53f98418e26ca85b79fd8444dd6f34ea5
Michał Worwąg <michalwo@gmail.com>
2025-04-21 15:01:33 +0200

    feat: update app title to include '(Test App)' for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div>
-      <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+      <div className="p-4 mb-4 bg-white rounded-lg shadow-sm">
+        <h1 className="text-xl font-bold text-center">
+          🚀 Rick and Morty - Fan Service (Test App)
+        </h1>
       </div>
       <Outlet />
     </div>
